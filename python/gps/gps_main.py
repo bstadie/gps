89c62cf7c960a2800a04cdccd4e8c624d10a81e6
Bradly Stadie <bstadie@berkeley.edu>
2016-04-13 20:24:04 -0700

    added in oracle learning files.

--- python/gps/gps_main.py
+++ python/gps/gps_main.py
@@ -212,6 +212,10 @@ class GPSMain(object):
 
     def _end(self):
         """ Finish running and exit. """
+        target = self._hyperparams['algorithm']['cost']['data_types'].values()[0]['target_state']
+        self.algorithm.policy_opt.policy.pickle_policy(self.agent.dO, self.agent.dU,
+                                                       self._data_files_dir + 'policies/',
+                                                       goal_state=target)
         if self.gui:
             self.gui.set_status_text('Training complete.')
             self.gui.end_mode()
